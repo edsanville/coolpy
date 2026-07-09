67a3e61bbc771db81fc26f684310a0e9e0e8148e
Ed Sanville <edsanville@gmail.com>
2026-07-09 22:48:54 +0800

    Actually USE a good Wikipedia User-Agent

--- src/coolpy/wikipedia/__init__.py
+++ src/coolpy/wikipedia/__init__.py
@@ -17,7 +17,7 @@ class Wikipedia:
 
     API_URL = "https://en.wikipedia.org/w/api.php"
     HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'CoolpyWikipediaBot/0.1.0 (https://www.sanvillesoftware.com; edsanville@gmail.com) coolpy/0.1.0'
     }
     session: CachedRequests
 
